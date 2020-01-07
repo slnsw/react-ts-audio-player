5de1cedc70493cc7f277576086815021917baf1d
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2020-01-07 13:14:03 +1100

    Implementing linting and formatting

--- src/TimeUtils.ts
+++ src/TimeUtils.ts
@@ -1,34 +1,26 @@
 // @file
 // Time utilities.
 
-export const toHHMMSS = (str: string): string => {
-  const secNum = parseInt(str, 10);
-  let hours   = Math.floor(secNum / 3600);
-  let minutes = Math.floor((secNum - (hours * 3600)) / 60);
-  let seconds = secNum - (hours * 3600) - (minutes * 60);
-
-  if (hours < 10) {
-    hours = "0" + hours;
+const strPadLeft = (n: number): string => {
+  if (n < 10) {
+    return `0${n}`;
   }
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-  return `${hours}:${minutes}:${seconds}`;
+  return n.toString();
 };
 
-const const toMMSS = (str: string): string => {
+export const toHHMMSS = (str: string): string => {
   const secNum = parseInt(str, 10);
-  let minutes = Math.floor(secNum / 60);
-  let seconds = secNum - (minutes * 60);
+  const hours   = Math.floor(secNum / 3600);
+  const minutes = Math.floor((secNum - (hours * 3600)) / 60);
+  const seconds = secNum - (hours * 3600) - (minutes * 60);
 
-  if (minutes < 10) {
-    minutes = "0" + minutes;
-  }
-  if (seconds < 10) {
-    seconds = "0" + seconds;
-  }
-  return `${minutes}:${seconds}`;
+  return `${strPadLeft(hours)}:${strPadLeft(minutes)}:${strPadLeft(seconds)}`;
+};
+
+export const toMMSS = (str: string): string => {
+  const secNum = parseInt(str, 10);
+  const minutes = Math.floor(secNum / 60);
+  const seconds = secNum - (minutes * 60);
+
+  return `${strPadLeft(minutes)}:${strPadLeft(seconds)}`;
 };
